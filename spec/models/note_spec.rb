d56bd73336a317c14d7628d1c26f7d0a14f1104b
Dai <kobayashi.daigo3@gmail.com>
2024-02-23 21:12:18 +0900

    feat: my-04-factoriesブランチとのコンフリクト解消

--- spec/models/note_spec.rb
+++ spec/models/note_spec.rb
@@ -1,66 +1,34 @@
 require 'rails_helper'
 
 RSpec.describe Note, type: :model do
-  before do
-    @user = User.create(
-      first_name: "Joe",
-      last_name:  "Tester",
-      email:      "joetester@example.com",
-      password:   "dottle-nouveau-pavilion-tights-furze",
-    )
-    @project = @user.projects.create(
-      name: "Test Project",
-    )
-  end
-
-  # ユーザー、プロジェクト、メッセージがあれば有効な状態であること
   it "is valid with a user, project, and message" do
-    note = Note.new(
-      message: "This is a sample note",
-      user: @user,
-      project: @project,
-    )
+    note = FactoryBot.create(:note)
     expect(note).to be_valid
   end
 
-  # メッセージがなければ無効な状態であること
   it "is invalid without a message" do
-    note = Note.new(message: nil)
+    note = FactoryBot.build(:note, message: nil)
     note.valid?
     expect(note.errors[:message]).to include("can't be blank")
   end
 
-  # 文字列に一致するメッセージを検索する
   describe "search message for a term" do
     before do
-      @note1 = @project.notes.create(
-        message: "This is the first note.",
-        user: @user,
-      )
-  
-      @note2 = @project.notes.create(
-        message: "This is the second note.",
-        user: @user,
-      )
-  
-      @note3 = @project.notes.create(
-        message: "First, preheat the oven.",
-        user: @user,
-      )
+      user = FactoryBot.create(:user)
+      project = FactoryBot.create(:project, owner: user)
+      @note1 = FactoryBot.create(:note, message: "This is the first note.", project: project)
+      @note2 = FactoryBot.create(:note, message: "This is the second note.", project: project)
+      @note3 = FactoryBot.create(:note, message: "First, preheat the oven.", project: project)
     end
 
-    # 一致するデータが見つかるとき
     context "when a match is found" do
-      # 検索文字列に一致するメモを返すこと
       it "returns notes that match the search term" do
         expect(Note.search("first")).to include(@note1, @note3)
       end
     end
 
-    # 一致するデータが1件も見つからないとき
     context "when no match is found" do
-      # 空のコレクションを返すこと
-      it "returns an empty collection when no results are found" do
+      it "returns an empty collection" do
         expect(Note.search("message")).to be_empty
       end
     end
--- spec/models/project_spec.rb
+++ spec/models/project_spec.rb
@@ -1,70 +1,49 @@
 require 'rails_helper'
 
 RSpec.describe Project, type: :model do
-  before do
-    @user = User.create(
-      first_name: "Joe",
-      last_name: "Tester",
-      email: "joetester@example.com",
-      password: "dottle-nouveau-pavilion-tights-furze",
-    )
-  end
-
-  # 名前のあるプロジェクトは有効な状態であること
-  it "is valid with a name" do
-    project = @user.projects.new(
-      name: "Named Project",
-    )
-    expect(project).to be_valid
-  end
-
-  # 名前のないプロジェクトは無効な状態であること
-  it "is invalid without a name" do
-    project_without_name = @user.projects.build(
-      name: nil,
-    )
-    project_without_name.valid?
-    expect(project_without_name.errors[:name]).to include("can't be blank")
-  end
-
-  # 重複したプロジェクト名を付ける
-  describe "name same project name to two projects" do
+  describe "validation of project's name" do
     before do
-      @project = @user.projects.create(
-        name: "Test Project",
-      )
+      @user = FactoryBot.create(:user)
+      FactoryBot.create(:project, name: "Test Project", owner: @user)
     end
 
-    # 一人のユーザーが同じ名前を使うとき
-    context "when one user use same project name to two project" do
-      # 許可されないこと
-      it "does not allow" do
-        second_project = @user.projects.build(
-          name: "Test Project",
-        )
-    
-        second_project.valid?
-        expect(second_project.errors[:name]).to include("has already been taken")
-      end
+    it "does not allow duplicate project names per user" do  
+      new_project = FactoryBot.build(:project, name: "Test Project", owner: @user)
+      new_project.valid?
+      expect(new_project.errors[:name]).to include("has already been taken")
+    end
+  
+    it "allows two users to share a project name" do
+      other_user = FactoryBot.create(:user)
+      other_project = FactoryBot.create(:project, name: "Test Project", owner: other_user)
+      expect(other_project).to be_valid
+    end
+  end
+
+  # 遅延ステータス
+  describe "late status" do
+    # 締切日が過ぎていれば遅延していること
+    it "is late when the due date is past today" do
+      project = FactoryBot.create(:project, :due_yesterday)
+      expect(project).to be_late
     end
 
-    # 二人のユーザーが同じ名前を使うとき
-    context "when two user use same project name to their own project" do
-      # 許可されること
-      it "allow" do
-        other_user = User.create(
-          first_name: "Jane",
-          last_name: "Tester",
-          email: "janetester@example.com",
-          password: "dottle-nouveau-pavilion-tights-furze",
-        )
-    
-        other_project = other_user.projects.build(
-          name: "Test Project",
-        )
-    
-        expect(other_project).to be_valid
-      end
+    # 締切日が今日ならスケジュールどおりであること
+    it "is on time when the due date is today" do
+      project = FactoryBot.create(:project, :due_today)
+      expect(project).to_not be_late
     end
+
+    # 締切日が未来ならスケジュールどおりであること
+    it "is on time when the due date is in the future" do 
+      project = FactoryBot.create(:project, :due_tomorrow)
+      expect(project).to_not be_late
+    end
+  end
+
+  # たくさんのメモがついていること
+  it "can have many notes" do
+    project = FactoryBot.create(:project, :with_notes)
+    expect(project.notes.length).to eq 5
   end
 end
