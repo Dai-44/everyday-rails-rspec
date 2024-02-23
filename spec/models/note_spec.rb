14428f78c6cb70a19d77f963d009cf6acda97a43
Dai <kobayashi.daigo3@gmail.com>
2024-02-23 19:30:33 +0900

    feat: 4. 意味のあるテストデータの作成(演習問題完了)

--- spec/models/note_spec.rb
+++ spec/models/note_spec.rb
@@ -1,48 +1,24 @@
 require 'rails_helper'
 
 RSpec.describe Note, type: :model do
-  before do
-    @user = User.create(
-      first_name: "Joe",
-      last_name:  "Tester",
-      email:      "joetester@example.com",
-      password:   "dottle-nouveau-pavilion-tights-furze",
-      )
-
-    @project = @user.projects.create(
-      name: "Test Project",
-      )
-  end
-
   it "is valid with a user, project, and message" do
-    note = Note.new(
-      message: "This is a sample note.",
-      user: @user,
-      project: @project,
-      )
+    note = FactoryBot.create(:note)
     expect(note).to be_valid
   end
 
   it "is invalid without a message" do
-    note = Note.new(message: nil)
+    note = FactoryBot.build(:note, message: nil)
     note.valid?
     expect(note.errors[:message]).to include("can't be blank")
   end
 
   describe "search message for a term" do
     before do
-      @note1 = @project.notes.create(
-        message: "This is the first note.",
-        user: @user,
-        )
-      @note2 = @project.notes.create(
-        message: "This is the second note.",
-        user: @user,
-        )
-      @note3 = @project.notes.create(
-        message: "First, preheat the oven.",
-        user: @user,
-        )
+      user = FactoryBot.create(:user)
+      project = FactoryBot.create(:project, owner: user)
+      @note1 = FactoryBot.create(:note, message: "This is the first note.", project: project)
+      @note2 = FactoryBot.create(:note, message: "This is the second note.", project: project)
+      @note3 = FactoryBot.create(:note, message: "First, preheat the oven.", project: project)
     end
 
     context "when a match is found" do
--- spec/models/project_spec.rb
+++ spec/models/project_spec.rb
@@ -1,50 +1,23 @@
 require 'rails_helper'
 
 RSpec.describe Project, type: :model do
-  it "does not allow duplicate project names per user" do
-    user = User.create(
-      first_name: "Joe",
-      last_name:  "Tester",
-      email:      "joetester@example.com",
-      password:   "dottle-nouveau-pavilion-tights-furze",
-      )
+  describe "validation of project's name" do
+    before do
+      @user = FactoryBot.create(:user)
+      FactoryBot.create(:project, name: "Test Project", owner: @user)
+    end
 
-    user.projects.create(
-      name: "Test Project",
-      )
-
-    new_project = user.projects.build(
-      name: "Test Project",
-      )
-
-    new_project.valid?
-    expect(new_project.errors[:name]).to include("has already been taken")
-  end
-
-  it "allows two users to share a project name" do
-    user = User.create(
-      first_name: "Joe",
-      last_name:  "Tester",
-      email:      "joetester@example.com",
-      password:   "dottle-nouveau-pavilion-tights-furze",
-      )
-
-    user.projects.create(
-      name: "Test Project",
-      )
-
-    other_user = User.create(
-      first_name: "Jane",
-      last_name:  "Tester",
-      email:      "janetester@example.com",
-      password:   "dottle-nouveau-pavilion-tights-furze",
-      )
-
-    other_project = other_user.projects.build(
-      name: "Test Project",
-      )
-
-    expect(other_project).to be_valid
+    it "does not allow duplicate project names per user" do  
+      new_project = FactoryBot.build(:project, name: "Test Project", owner: @user)
+      new_project.valid?
+      expect(new_project.errors[:name]).to include("has already been taken")
+    end
+  
+    it "allows two users to share a project name" do
+      other_user = FactoryBot.create(:user)
+      other_project = FactoryBot.create(:project, name: "Test Project", owner: other_user)
+      expect(other_project).to be_valid
+    end
   end
 
   # 遅延ステータス
