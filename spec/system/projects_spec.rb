2d250e45ab5d37eb3fe06639d66a590b9c5d9d87
Dai <kobayashi.daigo3@gmail.com>
2024-02-26 16:46:20 +0900

    fix: my-07-requestsとデフォルトブランチとのコンフリクト解消(my-07-requestsを修正)

--- spec/system/projects_spec.rb
+++ spec/system/projects_spec.rb
@@ -21,4 +21,48 @@ RSpec.describe "Projects", type: :system do
       expect(page).to have_content "Owner: #{user.name}"
     }.to change(user.projects, :count).by(1)
   end
+
+  context "edit a project" do
+    before do
+      @user = FactoryBot.create(:user)
+      FactoryBot.create(:project, name: "My Project", owner: @user)
+  
+      visit root_path
+      click_link "Sign in"
+      fill_in "Email", with: @user.email
+      fill_in "Password", with: @user.password
+      click_button "Log in"
+    end
+
+    scenario "user edits his project" do
+      expect {
+        click_link "My Project"
+        within "h1" do
+          click_link "Edit"
+        end
+        fill_in "Name", with: "Updated Project"
+        click_button "Update Project"
+  
+        expect(page).to have_content "Project was successfully updated."
+        expect(page).to have_content "Updated Project"
+        expect(page).to have_content "Owner: #{@user.name}"
+      }.to_not change(@user.projects, :count)
+    end
+
+    scenario "user can't edit his project with nil" do
+      expect {
+        click_link "My Project"
+        within "h1" do
+          click_link "Edit"
+        end
+        fill_in "Name", with: nil
+        click_button "Update Project"
+  
+        expect(page).to have_content "1 error prohibited this project from being saved"
+        expect(page).to have_content "Name can't be blank"
+        expect(page).to have_content "Editing project"
+        expect(page).to have_button "Update Project"
+      }.to_not change(@user.projects, :count)
+    end
+  end
 end
